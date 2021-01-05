d2c8fd3182149c885c606013d9ffe4305cbcaec8
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-01-05 16:18:51 +0100

    handle 404 error. close #14

---
+++ src/main/java/de/tekup/recipe/controllers/HandleExceptionController.java
@@ -0,0 +1,25 @@
+package de.tekup.recipe.controllers;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class HandleExceptionController {
+	
+	@ExceptionHandler(NoSuchElementException.class)
+	public ModelAndView handleNoSuchElementException(NoSuchElementException e) {
+		
+		ModelAndView modelAndView = new ModelAndView();
+		
+		modelAndView.setViewName("404error");
+		modelAndView.setStatus(HttpStatus.NOT_FOUND);
+		modelAndView.addObject("exception",e);
+		
+		return modelAndView;
+	}
+
+}
